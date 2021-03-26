0bb1196d7451768da8d26bca4deafb49c335cc64
Marouane Souda <marsou431@gmail.com>
2021-03-26 13:00:50 +0100

    test: switch getByText to getByRole at render header CallToAction.tsx

--- src/components/CallToAction/CallToAction.test.tsx
+++ src/components/CallToAction/CallToAction.test.tsx
@@ -4,8 +4,8 @@ import CallToAction from "./CallToAction";
 describe("testing CallToAction component", () => {
     test("renders header", () => {
         render(<CallToAction />);
-        const header = screen.getByText("Simple, traffic-based pricing");
-        expect(header).toBeInTheDocument();
+        const header = screen.getByRole("heading", { level: 2 });
+        expect(header).toHaveTextContent("Simple, traffic-based pricing");
     });
     test("renders paragraph", () => {
         render(<CallToAction />);
