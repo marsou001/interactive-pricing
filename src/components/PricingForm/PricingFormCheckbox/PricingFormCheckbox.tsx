40be4f7284ad19df59d055ff5aeabf13ffbe7aa6
Marouane Souda <marsou431@gmail.com>
2021-04-01 09:23:32 +0100

    refactor: remove curly braces from customCheckboxClick function

--- src/components/PricingForm/PricingFormCheckbox/PricingFormCheckbox.tsx
+++ src/components/PricingForm/PricingFormCheckbox/PricingFormCheckbox.tsx
@@ -39,9 +39,8 @@ function PricingFormCheckbox({
 }: PricingFormCheckboxProps): JSX.Element {
     const inputRef = useRef<HTMLInputElement>(null);
     const customCheckboxRef = useRef<HTMLDivElement>(null);
-    const customCheckboxClick = () => {
-        inputRef.current?.click();
-    };
+    
+    const customCheckboxClick = () => inputRef.current?.click();
 
     const handleHiddenInputClick = (): void => handleEvent();
 
